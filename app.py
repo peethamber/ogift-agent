fe8d1f2f312c926cc531b258a72a531094d5ad0e
peethamber <40764612+peethamber@users.noreply.github.com>
2025-04-12 18:01:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def gen_response(query):
 pdf = file_selector()
 st.write(" You selected ",pdf)
 
-if(pdf):
+if(pdf and pdf.endswith('.Pdf')):
     with open(pdf.title(), "rb") as f:
         pdf_data = base64.standard_b64encode(f.read()).decode("utf-8")
 query = st.text_input("Ask me anything from this book ")
