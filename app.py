20e0fa75e584b5d9a945f26dd216970ea5270307
peethamber <40764612+peethamber@users.noreply.github.com>
2025-04-12 18:58:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ if(pdf and pdf.endswith('.Pdf') or pdf.endswith('.pdf')):
     with open(pdf.title(), "rb") as f:
         pdf_data = base64.standard_b64encode(f.read()).decode("utf-8")
 else:
-        st.write("Choose a PDF document")
+        st.write("Choose a PDF document to run your query")
 query = st.text_input("Ask me anything from this book ")
 
 # display the name when the submit button is clicked
