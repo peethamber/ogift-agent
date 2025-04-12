f976295a93470883e926101c3855fb6d087f1d77
peethamber <40764612+peethamber@users.noreply.github.com>
2025-04-12 19:14:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ pdf = pdf.lower()
 st.write(" You selected ",pdf)
 
 if(pdf and pdf.endswith('.pdf')):
-    with open(pdf.title(), "rb") as f:
+    with open(pdf.title().lower(), "rb") as f:
         pdf_data = base64.standard_b64encode(f.read()).decode("utf-8")
 else:
         st.write("Choose a PDF document to run your query")
